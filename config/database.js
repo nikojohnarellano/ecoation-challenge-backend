1dfd12974f1d16a90d513af10a6b0ea8a2e9233b
Niko Arellano <narellano@my.bcit.ca>
2017-11-03 00:10:16 -0700

    added mongodb production connection string

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
     'secret' : 'niko-ecoation-challenge',
-    'database' : 'mongodb://localhost:27017/ecoation'
+    //'database' : 'mongodb://localhost:27017/ecoation'
+    'database' : 'mongodb://nikojohnarellano:NTn5UEkBf1QuwLuKksp0S8cSnxHtP1XmYEMsOIwrjC3pWgc3toyyycqy7HlVvX6yeNamNdFatAlg5hnXAMMCsg==@nikojohnarellano.documents.azure.com:10255/?ssl=true&replicaSet=globaldb'
 };
